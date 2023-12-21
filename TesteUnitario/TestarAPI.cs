9ccd4b7c903c91d442e2b15566d1082037e22c95
Edinaldo Sá <sazinho1@hotmail.com>
2023-12-21 17:41:30 -0300

    Fix : caso de teste

--- TesteUnitario/TestarAPI.cs
+++ TesteUnitario/TestarAPI.cs
@@ -20,25 +20,15 @@ namespace TesteUnitario
 
         }
 
-
-        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        [TestMethod]
         public async Task TestCovidApiSummary()
         {
             // URL da API
             string url = "https://dev.reserve.com.br/covid19api/summary";
-
             // Fazendo a requisição
             HttpResponseMessage response = await _httpClient.GetAsync(url);
-
-            // Verificando se a resposta é bem-sucedida
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(response.IsSuccessStatusCode, "A API não retornou um status de sucesso.");
 
-            // Opcional: Verifique a estrutura dos dados aqui
-
-
-
-            string responseData = await response.Content.ReadAsStringAsync();
-           // Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(VerificarEstruturaDosDados(responseData), "A estrutura dos dados não está correta.");
         }
 
 
