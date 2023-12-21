1696c0ff7b88dc9b9c1df8b8db36b8d38faf0031
Edinaldo Sá <sazinho1@hotmail.com>
2023-12-21 17:48:02 -0300

    Fix : testes

--- TesteUnitario/TestarAPI.cs
+++ TesteUnitario/TestarAPI.cs
@@ -1,5 +1,4 @@
-﻿using NUnit.Framework;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +28,20 @@ namespace TesteUnitario
             HttpResponseMessage response = await _httpClient.GetAsync(url);
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(response.IsSuccessStatusCode, "A API não retornou um status de sucesso.");
 
+        }
+
+
+        [TestMethod]
+        public async Task TestCovidApiSummaryClass()
+        {
+            // URL da API
+            string url = "https://dev.reserve.com.br/covid19api/summary";
+
+            // Fazendo a requisição
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            // Verificando se a resposta é bem-sucedida
+            Assert.IsTrue(response.IsSuccessStatusCode, "A API não retornou um status de sucesso.");
         }
 
 
